701a0e0d6c354dc7b398ca0fd1d2aa8411d9384a
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2025-11-15 17:49:05 -0600

    Test code pushed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleTest.java
@@ -4,19 +4,37 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Spindex Calibration TeleOp", group = "Calibration")
+@TeleOp
 public class TeleTest extends OpMode {
+    private Servo transferGate;
+    private DcMotorEx intakeMotor;
+    private DcMotorEx transferMotor;
+    private DcMotorEx turretMotor;
+    private DcMotorEx shooterMotor;
 
-    private DcMotorEx spindexMotor;
+    private double turretPower = 0;
 
     @Override
     public void init() {
-        spindexMotor = hardwareMap.get(DcMotorEx.class, "spindexMotor");
+        intakeMotor = hardwareMap.get(DcMotorEx.class, "intakeMotor");
+        transferMotor = hardwareMap.get(DcMotorEx.class, "transferMotor");
+        turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
+        shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
 
-        spindexMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        spindexMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        spindexMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        turretMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        turretMotor.setTargetPosition(0);
+        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        turretMotor.setPower(0);
+
+        transferGate = hardwareMap.get(Servo.class, "transferGate");
+
+        intakeMotor.setPower(0);
+        transferMotor.setPower(0);
+        shooterMotor.setPower(0);
 
         telemetry.addLine("Spindex Calibration Initialized");
         telemetry.addLine("Use left stick (Y) or triggers to rotate.");
@@ -25,36 +43,47 @@ public class TeleTest extends OpMode {
     }
 
     @Override
-    public void loop() {
-        double power = 0.0;
-
-        // Manual control: left stick up/down
-        power = -gamepad1.left_stick_y;
-
-        // Fine control: right trigger forward, left trigger backward
-        power += (gamepad1.right_trigger - gamepad1.left_trigger) * 0.5;
-
-        // Limit power range
-        power = Math.max(-1.0, Math.min(1.0, power));
-
-        spindexMotor.setPower(power);
-
-        // Reset encoder with A button
-        if (gamepad1.a) {
-            spindexMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            spindexMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
-        // Telemetry output
-        telemetry.addData("Power", "%.2f", power);
-        telemetry.addData("Encoder Position", spindexMotor.getCurrentPosition());
-        telemetry.addLine("Press A to reset encoder.");
-        telemetry.addLine("Rotate one full revolution, then record this value.");
-        telemetry.update();
+    public void start() {
+        super.start();
+        shooterMotor.setPower(1);
+        shooterMotor.setVelocity(0);
     }
 
     @Override
-    public void stop() {
-        spindexMotor.setPower(0);
+    public void loop() {
+        if (gamepad1.right_bumper) {
+            intakeMotor.setPower(1);
+            transferMotor.setPower(.7);
+        } else if (gamepad1.left_bumper) {
+            intakeMotor.setPower(-1);
+            transferMotor.setPower(-.7);
+        } else {
+            intakeMotor.setPower(0);
+            transferMotor.setPower(0);
+        }
+
+        if (gamepad1.circle) {
+            transferGate.setPosition(1);
+        } else {
+            transferGate.setPosition(0);
+        }
+
+        if (gamepad1.dpad_up) {
+            shooterMotor.setVelocity(1200);
+        } else {
+            shooterMotor.setVelocity(0);
+        }
+
+        turretPower = gamepad1.right_trigger - gamepad1.left_trigger;
+
+        if (Math.abs(turretPower) > 0) {
+            turretMotor.setPower(turretPower);
+            turretMotor.setTargetPosition(turretMotor.getCurrentPosition() + (int) (25 * (turretPower)));
+        }
+
+        // Telemetry output
+        telemetry.addData("Shooter velocity: ", shooterMotor.getVelocity());
+        telemetry.addData("Turret Position: ", turretMotor.getCurrentPosition());
+        telemetry.update();
     }
 }
